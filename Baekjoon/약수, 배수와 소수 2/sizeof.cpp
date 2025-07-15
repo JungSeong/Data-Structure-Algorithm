beb3d6db425012af99f22e7937e9e9213147f226
jungseong <jungseonglian@sju.ac.kr>
2025-07-15 12:17:37 +0900

    250715

---
+++ Baekjoon/약수, 배수와 소수 2/sizeof.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main() {
+    cout << "int:      " << INT_MIN << " ~ " << INT_MAX << '\n';
+    cout << "long:     " << LONG_MIN << " ~ " << LONG_MAX << '\n';
+    cout << "llong:    " << LLONG_MIN << " ~ " << LLONG_MAX << '\n';
+    return 0;
+}
