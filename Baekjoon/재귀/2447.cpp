a16cbb542617f0b9c375ce8f9a802239adca5aa9
JungSeong <jungseonglian@sju.ac.kr>
2025-09-09 11:26:36 +0900

    250909

--- Baekjoon/재귀/2447.cpp
+++ Baekjoon/재귀/2447.cpp
@@ -6,38 +6,39 @@ using namespace std;
 
 char pattern[3][3] = {{'*','*','*'},{'*',' ','*'},{'*','*','*'}};
 
-void RecursivePattern(int idx, int length, int full, vector<vector<bool>>& isBlank)
+void definePattern(int row, int col, int length, vector<vector<bool>>& isBlank)
 {
     if (length > 3)
     {
-        for (int i=idx; i<idx+length/3; i++)
+        for (int i=row; i<row+length/3; i++)
         {
-            for (int ii=idx; ii<idx+length/3; ii++)
+            for (int ii=col; ii<col+length/3; ii++)
             {
                 isBlank[i][ii] = true;
             }
         }
 
-        RecursivePattern(idx-length/9*2, length/3, full, isBlank);
-        RecursivePattern(idx+length/9, length/3, full, isBlank);
-        RecursivePattern(idx+length/3+length/9, length/3, full, isBlank);
+        for (int i=0; i<3; i++)
+        {
+            for (int ii=0; ii<3; ii++)
+            {
+                definePattern(i*3, ii*3, length/3, isBlank);
+            }
+        }
     }
 }
 
 int main()
 {
-    int N;
+    int N; 
     cin >> N;
-    
-    double length = pow(3,N);
-    double full = length;
 
-    vector<vector<bool>> isBlank(length, vector<bool>(length, false));
-    RecursivePattern(length/3, length, full, isBlank);
+    vector<vector<bool>> isBlank(N, vector<bool>(N, false));
+    definePattern(N/3, N/3, N, isBlank);
 
-    for (int i=0; i<length; i++)
+    for (int i=0; i<N; i++)
     {
-        for (int ii=0; ii<length; ii++)
+        for (int ii=0; ii<N; ii++)
         {
             cout << isBlank[i][ii];
         }
