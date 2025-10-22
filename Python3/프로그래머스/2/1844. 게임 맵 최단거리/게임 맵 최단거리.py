29d59c78df342a35764ee2b521014270bac6fca2
Seongwon Jang <jungseonglian@sju.ac.kr>
2025-10-22 17:48:59 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.79 ms, Memory: 9.31 MB -BaekjoonHub

--- Python3/프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ Python3/프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,38 +1,40 @@
-# 최단거리 문제 -> BFS (큐로 구현)
+# 최단거리 -> BFS로 풀이할 것
 
 from collections import deque
 
-def bfs(i, j, n, m, cnt, maps, visited) :
-    q = deque([(cnt, i, j)])
+def BFS(i, j, cnt, maps, visited, m, n) :
+    q = deque([(i, j, cnt)])
+    move_list = ([1,0], [-1,0], [0,1], [0,-1])
     visited[i][j] = True
     
-    delta = [(1,0), (-1,0), (0,1), (0,-1)]
-    
     while q :
-        cnt, x, y = q.popleft()
-        
-        if x == n-1 and y == m-1 : return cnt
-        
-        for dx, dy in delta :
-            nx = x + dy
-            ny = y + dx
-            
-            if 0 <= nx < n and 0 <= ny < m and visited[nx][ny] is False and maps[nx][ny] == 1 :
-                q.append((cnt+1, nx, ny))
-                visited[nx][ny] = True
+        cur_i, cur_j, cnt = q.popleft()
 
+        if cur_i == n-1 and cur_j == m-1 : return cnt
+
+        for move in move_list :
+            dx, dy = move
+
+            nx = cur_i + dx
+            ny = cur_j + dy
+
+            if 0 <= nx < n and 0 <= ny < m and visited[nx][ny] == False and maps[nx][ny] == 1 :
+                q.append([nx, ny, cnt+1])
+                visited[nx][ny] = True
+                
 def solution(maps):
-    answer = -1
+    answer = 0
     i = 0
     j = 0
+    cnt = 1
     
-    n = len(maps) # 행의 크기
-    m = len(maps[0]) # 열의 크기
+    m = len(maps[0])
+    n = len(maps)
     
     visited = [[False for _ in range(m)] for _ in range(n)]
     
-    result = bfs(i,j,n,m,1,maps,visited)
-    if result is None : answer = -1
-    else : answer = result
+    cnt = BFS(i, j, cnt, maps, visited, m, n)
     
-    return answer
+    if cnt == None : cnt = -1
+    
+    return cnt
