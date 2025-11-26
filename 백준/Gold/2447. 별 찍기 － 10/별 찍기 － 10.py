1e437157ffbe6c3503c92c0b4659d28a83e803e6
Seongwon Jang <jungseonglian@sju.ac.kr>
2025-11-26 11:02:16 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 840 ms, Memory: 68776 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
@@ -2,21 +2,20 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-m = [[' ' for _ in range(N)] for _ in range(N)]
+m = [[' ']*N for _ in range(N)]
 
-def print_star(x, y, N) :
+def print_star(row, col, N) :
     if N == 1 :
-        m[x][y] = "*"
-        return
-    n = N // 3
-    for i in range(3) :
-        for j in range(3) :
-            if i == 1 and j == 1 :
-                continue
-            else :
-                print_star(x+n*i, y+n*j, N//3)
+        m[row][col] = '*'
+    else :
+        for i in range(3) :
+            for j in range(3) :
+                if i == 1 and j == 1 :
+                    continue
+                else :
+                    print_star(row+i*N//3, col+j*N//3, N//3)
 
-print_star(0, 0, N)
+print_star(0,0,N)
 
 for row in m :
     print(''.join(row))
