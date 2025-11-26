3f50a65fbf3c2d751e4b42079ebe6d32c6fc349f
Seongwon Jang <jungseonglian@sju.ac.kr>
2025-11-26 13:03:26 +0900

    [Silver I] Title: 미로 탐색, Time: 68 ms, Memory: 35076 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,14 +1,14 @@
-from collections import deque
 import sys
-
+from collections import deque
 input = sys.stdin.readline
+
 N, M = map(int, input().split())
 m = [list(map(int, input().strip())) for _ in range(N)]
-move = [[-1,0],[0,1],[1,0],[0,-1]] #상,우,하,좌
+move = [[-1,0],[0,1],[1,0],[0,-1]]
 
-def BFS(r, c) :
+def BFS() :
     q = deque()
-    q.append([r, c])
+    q.append([0,0])
 
     while q :
         cur_r, cur_c = q.popleft()
@@ -16,10 +16,10 @@ def BFS(r, c) :
             print(m[cur_r][cur_c])
             break
         for row in move :
-            next_r = cur_r + row[0]
-            next_c = cur_c + row[1]
-            if 0<=next_r<N and 0<=next_c<M and m[next_r][next_c] == 1 :
-                m[next_r][next_c] = m[cur_r][cur_c] + 1
-                q.append([next_r,next_c])
+            new_r, new_c = cur_r + row[0], cur_c + row[1]
+            if 0<=new_r<N and 0<=new_c<M :
+                if m[new_r][new_c] == 1 :
+                    m[new_r][new_c] = m[cur_r][cur_c] + 1
+                    q.append([new_r, new_c])
 
-BFS(0,0)
+BFS()
