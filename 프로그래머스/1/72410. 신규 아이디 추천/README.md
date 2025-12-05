87fa11c2257ea802b49c0f8369ac3594821a8c59
Seongwon Jang <jungseonglian@sju.ac.kr>
2025-12-05 12:38:20 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.32 ms, Memory: 9.37 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/README.md
+++ 프로그래머스/1/72410. 신규 아이디 추천/README.md
@@ -1,10 +1,10 @@
 # [level 1] 신규 아이디 추천 - 72410 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72410) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72410#) 
 
 ### 성능 요약
 
-메모리: 9.32 MB, 시간: 0.53 ms
+메모리: 9.37 MB, 시간: 0.32 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 12월 04일 14:26:29
+2025년 12월 05일 12:38:17
 
 ### 문제 설명
 
--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -1,57 +1,19 @@
+import re
+
 def solution(new_id):
     answer = ''
-    first = new_id.lower()
-    available_ch = ['-','_','.']
+    new_id = new_id.lower()
+    new_id = re.sub('[^a-z0-9\-_.]', '', new_id)
+    new_id = re.sub('[.]+', '.', new_id)
+    new_id = re.sub('^[.]|[.]$', '', new_id)
+    if len(new_id) == 0 :
+        new_id = 'a'
+    if len(new_id) >= 16 :
+        new_id = new_id[:15]
+        new_id = re.sub('[.]$', '', new_id)
+    if len(new_id) <= 2 :
+        while len(new_id) < 3 :
+            new_id += new_id[-1]
     
-    second = ''
-    for ch in first :
-        if not ch.isalpha() and ch in available_ch :
-            second += ch
-        elif ch.isalpha() or ch.isdigit() :
-            second += ch
-            
-    print(second)
-            
-    third = ''
-    for i in range(len(second)) :
-        if len(third) != 0 and third[-1] == '.' and second[i] == '.' :
-            continue
-        else :
-            third += second[i]
-    
-    print(third)
-
-    fourth = ''
-    for i in range(len(third)) :
-        if i == 0 or i == len(third)-1 :
-            if third[i] == '.' :
-                continue
-            else :
-                fourth += third[i]
-        else :
-            fourth += third[i]
-            
-    print(fourth)
-            
-    fifth = ''
-    if fourth == '' :
-        fifth = fourth + 'a'
-    else :
-        fifth = fourth
-        
-    print(fifth)
-    
-    sixth = fifth
-    if len(fifth) >= 16 :
-        sixth = fifth[:15]
-        if sixth[-1] == '.' :
-            sixth = sixth[:-1]
-    
-    seventh = sixth
-    if len(seventh) <= 2 :
-        while len(seventh) < 3 :
-            seventh += seventh[-1]
-            
-    answer = seventh
-    
+    answer = new_id    
     return answer
