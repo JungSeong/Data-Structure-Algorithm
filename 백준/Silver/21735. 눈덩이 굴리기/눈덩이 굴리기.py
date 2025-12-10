3d9e3f43ebbe6abe0851d114cae9070bfdebdbd5
Seongwon Jang <jungseonglian@sju.ac.kr>
2025-12-10 11:05:41 +0900

    [Silver III] Title: 눈덩이 굴리기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/21735. 눈덩이 굴리기/눈덩이 굴리기.py
+++ 백준/Silver/21735. 눈덩이 굴리기/눈덩이 굴리기.py
@@ -2,19 +2,18 @@ import sys
 INPUT = sys.stdin.readline
 
 N, M = map(int, INPUT().split())
-a = [0]
-a.extend(list(map(int, input().split())))
-move = [1, 2]
+a = [0] + list(map(int, INPUT().split()))
+move = [1,2]
 
 def BackTracking(cur, t, s) :
     if cur == N or t == M :
         return s
-    answer = s
-    for row in move :
-        if row == 1 and 0<=cur+1<=N:
-            answer = max(answer, BackTracking(cur+1, t+1, s+a[cur+1]))
-        elif row == 2 and 0<=cur+2<=N:
-            answer = max(answer, BackTracking(cur+2,t+1,s//2+a[cur+2]))
+    answer = -1
+    for i in range(2) :
+        if i == 0 and 0<=cur+1<N+1 :
+            answer = max(BackTracking(cur+1, t+1, s+a[cur+1]), answer)
+        elif i == 1 and 0<=cur+2<N+1 :
+            answer = max(BackTracking(cur+2, t+1, s//2+a[cur+2]), answer)
     return answer
 
-print(BackTracking(0,0,1))
+print(BackTracking(0, 0, 1))
