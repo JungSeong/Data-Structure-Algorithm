331b4ab35a888adc09d1dbb7af469f9cb5f352b7
Seongwon Jang <jungseonglian@sju.ac.kr>
2025-12-11 09:36:36 +0900

    [Silver III] Title: N과 M (2), Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -3,11 +3,6 @@ from itertools import combinations
 INPUT = sys.stdin.readline
 
 N, M = map(int, INPUT().split())
-l = []
-for i in range(1, N+1) :
-    l.append(i)
-
-comb = combinations(l, M)
-
-for row in comb :
-    print(' '.join(map(str, row)))
+possible_combinations = combinations(range(1,N+1),M)
+for row in possible_combinations :
+    print(*row)
