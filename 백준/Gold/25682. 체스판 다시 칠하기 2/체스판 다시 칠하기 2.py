a88ce0c6a9e8e768ebcd99fb5971fa864d439dea
Seongwon Jang <jungseonglian@sju.ac.kr>
2026-01-23 20:19:10 +0900

    [Gold IV] Title: 체스판 다시 칠하기 2, Time: 752 ms, Memory: 332448 KB -BaekjoonHub

---
+++ 백준/Gold/25682. 체스판 다시 칠하기 2/체스판 다시 칠하기 2.py
@@ -0,0 +1,50 @@
+import sys
+input = sys.stdin.readline
+
+N, M, K = map(int, input().split())
+m = list(list(input().strip()) for _ in range(N))
+
+case1 = [[0]*(M+1) for _ in range(N+1)] #BWBWBWBW
+case2 = [[0]*(M+1) for _ in range(N+1)] #WBWBWBWB
+
+for i in range(1, N+1) :
+    for j in range(1, M+1) :
+        if i%2!=0 :
+            if j%2!=0 :
+                if m[i-1][j-1] == "B" :
+                    case1[i][j] += case1[i-1][j] + case1[i][j-1] - case1[i-1][j-1]
+                    case2[i][j] += case2[i-1][j] + case2[i][j-1] - case2[i-1][j-1] + 1
+                else :
+                    case1[i][j] += case1[i-1][j] + case1[i][j-1] - case1[i-1][j-1] + 1
+                    case2[i][j] += case2[i-1][j] + case2[i][j-1] - case2[i-1][j-1]
+            else :
+                if m[i-1][j-1] == "W" :
+                    case1[i][j] += case1[i-1][j] + case1[i][j-1] - case1[i-1][j-1]
+                    case2[i][j] += case2[i-1][j] + case2[i][j-1] - case2[i-1][j-1] + 1
+                else :
+                    case1[i][j] += case1[i-1][j] + case1[i][j-1] - case1[i-1][j-1] + 1
+                    case2[i][j] += case2[i-1][j] + case2[i][j-1] - case2[i-1][j-1]
+        else :
+            if j%2!=0 :
+                if m[i-1][j-1] == "W" :
+                    case1[i][j] += case1[i-1][j] + case1[i][j-1] - case1[i-1][j-1]
+                    case2[i][j] += case2[i-1][j] + case2[i][j-1] - case2[i-1][j-1] + 1
+                else :
+                    case1[i][j] += case1[i-1][j] + case1[i][j-1] - case1[i-1][j-1] + 1
+                    case2[i][j] += case2[i-1][j] + case2[i][j-1] - case2[i-1][j-1]
+            else :
+                if m[i-1][j-1] == "B" :
+                    case1[i][j] += case1[i-1][j] + case1[i][j-1] - case1[i-1][j-1]
+                    case2[i][j] += case2[i-1][j] + case2[i][j-1] - case2[i-1][j-1] + 1
+                else :
+                    case1[i][j] += case1[i-1][j] + case1[i][j-1] - case1[i-1][j-1] + 1
+                    case2[i][j] += case2[i-1][j] + case2[i][j-1] - case2[i-1][j-1]
+
+answer = float('inf')
+
+for i in range(N-K+1) :
+    for j in range(M-K+1) :
+        comp = min(case1[i+K][j+K]-case1[i+K][j]-case1[i][j+K]+case1[i][j], case2[i+K][j+K]-case2[i+K][j]-case2[i][j+K]+case2[i][j])
+        answer = min(answer, comp)
+
+print(answer)
