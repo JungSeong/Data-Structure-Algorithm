baab1718ae257637737c18282b7c82557cc20412
Seongwon Jang <jungseonglian@sju.ac.kr>
2026-01-27 15:36:51 +0900

    [Silver V] Title: 행렬 곱셈, Time: 92 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/2740. 행렬 곱셈/행렬 곱셈.py
+++ 백준/Silver/2740. 행렬 곱셈/행렬 곱셈.py
@@ -3,15 +3,18 @@ input = sys.stdin.readline
 
 N, M = map(int, input().split())
 A = list(list(map(int, input().split())) for _ in range(N))
+
 M, K = map(int, input().split())
 B = list(list(map(int, input().split())) for _ in range(M))
-B_T = list(zip(*B))
+B_T = list(map(list, zip(*B)))
 
-for i in range(N) :
-    answer = []
-    for j in range(K) :
-        C_j = 0
-        for k in range(M) :
-            C_j += A[i][k] * B_T[j][k]
-        answer.append(C_j)
-    print(*answer)
+answer = []
+
+for row in A :
+    l = []
+    for col in B_T :
+        l.append(sum(map(lambda x,y : x*y, row, col)))
+    answer.append(l)
+
+for row in answer :
+    print(*row)
